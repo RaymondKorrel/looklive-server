22dc6c88806541f068d152e1705f3cae8dbea70b
Raymond <raymondkorrel@hotmail.com>
2016-03-07 17:07:37 +0100

    html css bem

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $('.product').first().addClass('product-active');
-    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('product-indicator-active');
+    $('.main__appearance__info__products__inner__product').first().addClass('main__appearance__info__products__inner__product-active');
+    $(".main__appearance__img__product-indicator[data-uuid='"+$('.main__appearance__info__products__inner__product').first().data('uuid')+"']").addClass('main__appearance__img__product-indicator-active');
 
-    $('.product-indicator').on('click', function(e){
+    $('.main__appearance__img__product-indicator').on('click', function(e){
         var id = $(e.currentTarget).data('uuid');
-        $('.product-indicator-active').removeClass('product-indicator-active');
-        $(e.currentTarget).addClass('product-indicator-active');
-        $('.product.product-active').removeClass('product-active');
-        $(".product[data-uuid='"+id+"']").addClass('product-active');
+        $('.main__appearance__img__product-indicator-active').removeClass('main__appearance__img__product-indicator-active');
+        $(e.currentTarget).addClass('main__appearance__img__product-indicator-active');
+        $('.main__appearance__info__products__inner__product.main__appearance__info__products__inner__product-active').removeClass('main__appearance__info__products__inner__product-active');
+        $(".main__appearance__info__products__inner__product[data-uuid='"+id+"']").addClass('main__appearance__info__products__inner__product-active');
     });
 });
